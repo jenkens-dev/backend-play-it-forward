71a2df468364ed233c6e76b36f7a152773f042f4
Taljjaa <jenken131@gmail.com>
2019-12-30 09:28:51 -0800

    added model associations, started seed file, and fixed schema

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,4 @@
 class Organization < ApplicationRecord
+    has_many :event_organizations 
+    has_many :events, through: :event_organization 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+event1 = Event.create(title: "Hacky New Year", date: , location:'Microsoft Reactor (Building 20) ,3709 Microsoft Way Redmond, WA 98052', description: 'Join us on January 11th for a convening of the local tech-for-good community.')
+org1 = Organization.create(username: 'Demolab', display_name: 'Democracy Lab', address: '', logo: 'https://d1agxr2dqkgkuy.cloudfront.net/img/dl_logo.png', )
