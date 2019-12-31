fab159a94dc155827aa7511b0e93edd3c65a2bee
Taljjaa <jenken131@gmail.com>
2019-12-30 17:53:03 -0800

    fixed backend associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
     has_many :event_volunteers
     has_many :event_organizations
-    has_many :organizations, through: :event_organization 
+    has_many :organizations, through: :event_organizations 
     has_many :volunteers, through: :event_volunteers
 end
