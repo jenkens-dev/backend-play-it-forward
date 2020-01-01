0981c613c1abd39686112c8a04817c6b9cdfc8f5
Taljjaa <jenken131@gmail.com>
2019-12-31 19:03:33 -0800

    passed associated organizations back to the frontend and started on volunteer controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
     def index
         events = Event.all 
-        render json: events
+        render json: events, include: [:organization]
     end
 end
--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,2 +1,5 @@
 class VolunteersController < ApplicationController
+    def create 
+        byebug
+    end
 end
