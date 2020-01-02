567e65530a43d9cca925bb195b5ca5fee4eee72d
Taljjaa <jenken131@gmail.com>
2020-01-01 20:35:07 -0800

    created show route in event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,9 @@ class EventsController < ApplicationController
         events = Event.all 
         render json: events, include: [:organization]
     end
+
+    def show 
+        event = Event.find(params[:id])
+        render json: event, include: [:organization, :volunteers]
+    end
 end
--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,16 +1,16 @@
 class VolunteersController < ApplicationController
     def create 
-        @volunteer = Volunteer.new({username: params[:username]})
-        if @volunteer.valid? 
-            @volunteer.save
-            render json: @volunteer
+        volunteer = Volunteer.new({username: params[:username]})
+        if volunteer.valid? 
+            volunteer.save
+            render json: volunteer
         end
     end
 
     def login
-        @volunteer = Volunteer.find_by({username: params[:username]})
-        if @volunteer
-            render json: @volunteer
+        volunteer = Volunteer.find_by({username: params[:username]})
+        if volunteer
+            render json: volunteer
         end
     end
 end
