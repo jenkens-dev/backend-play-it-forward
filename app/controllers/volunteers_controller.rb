c654d966a78c60e2f6c65e89813b5657e5499c78
Taljjaa <jenken131@gmail.com>
2020-01-02 08:23:45 -0800

    added show logic and destory to volunteer

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -7,10 +7,21 @@ class VolunteersController < ApplicationController
         end
     end
 
+    def show 
+        volunteer = Volunteer.find(params[:id])
+        render json: volunteer, include: [:events]
+    end
+
     def login
         volunteer = Volunteer.find_by({username: params[:username]})
         if volunteer
             render json: volunteer
         end
+    end
+
+    def destroy 
+        volunteer = Volunteer.find(params[:id])
+        volunteer.destroy
+        render json: Volunteer.all
     end
 end
